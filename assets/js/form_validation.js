962deb7d5a4c1f13b64f4226ceb4fbf26abdced0
SadhanaG <sadhana.gogoi1@gmail.com>
2015-06-23 17:35:43 +0530

    Validation changes
    Temp. change in those file.

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -1,20 +1,20 @@
-// JavaScript Document
-window.onload = init;
-
-function init(){
-	document.getElementById("int_form").onsubmit = validateForm;
-   	document.getElementById("btnReset").onclick = clearForm;
-   	document.getElementById("txtName").focus();
-}
-
-function validateForm(theForm) {
-   with(theForm) {
-      
-      return (isNotEmpty(txtName, "Please enter your name!", elmNameError)
-      );
-   }
-   
-   // Set initial focus
-   document.getElementById("txt_c_name").focus();
-}
- 
+$(document).foundation({
+    abide: {
+        validators: {
+            checkbox_limit: function(el, required, parent) {
+                var group = parent.closest( '.checkbox-group' );
+                var limit = group.attr('data-abide-validator-limit').split(',');
+                var countC = group.find(':checked').length;
+                if( countC >= limit[0] && countC <= limit[1] ) {
+                    group.find('small.error').hide();
+                    //return true so abide can clear any invalid flags on this element
+                    return true;
+                } else {
+                    group.find('small.error').css({display:'block'});
+                    //return false and let abide do its thing to make sure the form doesn't submit
+                    return false;
+                }
+            }
+        }
+    }
+});
