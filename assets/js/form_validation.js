4db51016a4d2f50abaf413f893be6f52e1104a72
SadhanaG <sadhana.gogoi1@gmail.com>
2015-06-25 14:39:51 +0530

    ZURB validation

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -1,20 +1,8 @@
-$(document).foundation({
-    abide: {
-        validators: {
-            checkbox_limit: function(el, required, parent) {
-                var group = parent.closest( '.checkbox-group' );
-                var limit = group.attr('data-abide-validator-limit').split(',');
-                var countC = group.find(':checked').length;
-                if( countC >= limit[0] && countC <= limit[1] ) {
-                    group.find('small.error').hide();
-                    //return true so abide can clear any invalid flags on this element
-                    return true;
-                } else {
-                    group.find('small.error').css({display:'block'});
-                    //return false and let abide do its thing to make sure the form doesn't submit
-                    return false;
-                }
-            }
-        }
-    }
-});
+$('#myForm')
+  .on('invalid.fndtn.abide', function () {
+    var invalid_fields = $(this).find('[data-invalid]');
+    console.log(invalid_fields);
+  })
+  .on('valid.fndtn.abide', function () {
+    console.log('valid!');
+  });
