507e6cd261e48293541f335a3a307a1c1f76e59f
SadhanaG <sadhana.gogoi1@gmail.com>
2015-07-03 17:42:36 +0530

    Changes in path

---
+++ public/assets/js/form_validation.js
@@ -0,0 +1,8 @@
+$('#myForm')
+  .on('invalid.fndtn.abide', function () {
+    var invalid_fields = $(this).find('[data-invalid]');
+    console.log(invalid_fields);
+  })
+  .on('valid.fndtn.abide', function () {
+    console.log('valid!');
+  });
